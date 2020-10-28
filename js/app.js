7c3c18af0844667703f135e398f551a22bbdb7d9
tekthree <tekthree@gmail.com>
2020-10-28 16:08:21 -0700

    Adding in the guessing game JS

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 'use strict'
+// keeping score of all the correct answers
+var score =0;
 
 //greeting
 alert('What is up, my name is Tek');
@@ -58,6 +60,38 @@ if(myGoal.toLowerCase() === 'no' || myGoal.toLowerCase() === 'n') {
     // console.log('wrong');
     alert('Dang, you must think I\'m crazy, WoW');
 
+}
+alert('you have 10 guesses. Guess 1-10');
+var answer = 3;
+var guess = prompt('what is your guess?');
+for (var i = 0; i < 3; i++) {
+  if (answer == guess) {
+    alert('you guessed correctly');
+    break;
+  } else if (guess < answer) {
+    guess = prompt('Too low. Please try again');
+  } else if (guess > answer) {
+    guess = prompt('Too high. Please try a lower number');
+  } else if (i === 2) {
+    alert('Dang dude, wrong answer. The correct answer is 3.');
+    break;
+  }
+}
+
+var countriesVisited = ['japan', 'iraq', 'thailand'];
+for (i = 0; i < 6; i++) {
+  var countryGuess = prompt('Let\'s play a game. Can you guess any of the countries I have lived in in the past 15 years').toLowerCase();
+  if (countryGuess === countriesVisited[0] ||
+    countryGuess === countriesVisited[1] ||
+    countryGuess === countriesVisited[2]) {
+    score++;
+    alert('You\'re right. I have been to a lot of different countries.');
+    break;
+  } else if (i === 5) {
+    alert('Sorry. The correct answers are Japan, Iraq or Thailand');
+  } else {
+    alert('Wrong. Try again.');
+  }
 }
 
 //Thanks for playing
